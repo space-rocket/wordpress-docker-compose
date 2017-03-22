4e99cf3be801e74f3668a902475c86cb45834850
Michael Chavez <bigbassroller@gmail.com>
2017-03-22 13:47:50 -0700

    added basic auth

--- wp-angular/src/app/users/users.service.ts
+++ wp-angular/src/app/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 import { User } from './user';
 
@@ -13,14 +14,57 @@ export class UsersService {
 
 	private usersUrl = "http://localhost/wp-json/wp/v2/";
 
+	private extractData(res: Response) {
+	  let body = res.json();
+	  return body.data || { };
+	}
+
+	private handleError (error: Response | any) {
+		// In a real world app, you might use a remote logging infrastructure
+		let errMsg: string;
+		if (error instanceof Response) {
+			const body = error.json() || '';
+			const err = body.error || JSON.stringify(body);
+			errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+		} else {
+			errMsg = error.message ? error.message : error.toString();
+		}
+		console.error(errMsg);
+		return Observable.throw(errMsg);
+	}
+
+
 	constructor(private http: Http) { }
 
-	getUsers(): Observable<User[]> {
+	createAuthorizationHeader(headers: Headers) {
+		headers.append('Authorization', 'Basic ' +
+			btoa('localhost:localhost')); 
+	}
 
-		return this.http
-		.get(this.usersUrl + 'users')
+
+
+	getUsers(users: User[]): Observable<User[]> {
+		let headers = new Headers();
+		this.createAuthorizationHeader(headers);
+
+		return this.http.get(this.usersUrl + 'users', {
+	      headers: headers
+	    })
 		.map((res: Response) => res.json());
 
+	}
+	// getUsers(): Observable<User[]> {
+	//   return this.http.get(this.usersUrl)
+	//                   .map(this.extractData)
+	//                   .catch(this.handleError);
+	// }
+	create(name: string): Observable<User> {
+		let headers = new Headers({ 'Content-Type': 'application/json' });
+		let options = new RequestOptions({ headers: headers });
+
+		return this.http.post(this.usersUrl, { name }, options)
+		.map(this.extractData)
+		.catch(this.handleError);
 	}
 
 }
